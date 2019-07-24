ec87e4305e9451682a363e5e6b5efb690f508301
anwarkadir76 <49220464+anwarkadir76@users.noreply.github.com>
2019-07-24 12:00:42 -0400

    First Java Code Commit

---
+++ infoteksolution/src/infoteksolution/FirstJavaCode.java
@@ -0,0 +1,10 @@
+package infoteksolution;
+
+public class FirstJavaCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome to Software Automation Testing");
+	}
+
+}
